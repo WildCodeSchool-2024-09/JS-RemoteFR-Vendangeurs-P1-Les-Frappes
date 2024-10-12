49f1ce9ab4aee8db8adf291e48979f62e710f086
Foxrim <Flavien085@gmail.com>
2024-10-12 11:31:48 +0200

    [Feat] slide version 1

--- scripts/cards.js
+++ scripts/cards.js
@@ -1,35 +1,41 @@
 import restaurant from "./restautant";
 
-let card = document.querySelector("#card");
+// let card = document.querySelector("#card");
 
-card = `
-    <img class="restaurantImg" src="${restaurant[0].img}" alt="Restaurant">
-    <h2>${restaurant[0].name}</h2>
-    <h3>"Note"</h3>
-    <img src="${restaurant[0].note}" alt="note_etoiles"> 
-    <p>${restaurant[0].notice}</p>
-    <hr>
-    <p>${restaurant[0].desc}</p>
-`
+// card = `
+//     <img class="restaurantImg" src="${restaurant[0].img}" alt="Restaurant">
+//     <h2>${restaurant[0].name}</h2>
+//     <h3>"Note"</h3>
+//     <img src="${restaurant[0].note}" alt="note_etoiles"> 
+//     <p>${restaurant[0].notice}</p>
+//     <hr>
+//     <p>${restaurant[0].desc}</p>
+// `
 
-// J'ai oublié comment c'est le reste
+// // J'ai oublié comment c'est le reste
 
-for (i = 0; i < restaurant.lenght; i++ ) { 
+// for (i = 0; i < restaurant.lenght; i++ ) { 
 
-}
+// }
 
-card.outerHTML = card; 
+// card.outerHTML = card; 
 
 
 const buttons = document.querySelectorAll(".btn"); 
-const slide = document.querySelectorAll(".card");
+const slides = document.querySelectorAll(".card");
 
 buttons.forEach((button) => {
     button.addEventListener("click", (e) => {
-        const calcNextSlide = e.target.id === "next" ? 1 : -1;
-        const slideActive = document.querySelector(".active");
-
-        console.log([slide]);
+      const calcNextSlide = e.target.id === "next" ? 1 : -1;
+      const slideActive = document.querySelector(".active");
+  
+      newIndex = calcNextSlide + [...slides].indexOf(slideActive);
+  
+      if (newIndex < 0) newIndex = [...slides].length - 1;
+      if (newIndex >= [...slides].length) newIndex = 0;
+      slides[newIndex].classList.add("active");
+        
+      console.log("calcNextSlide");
+      slideActive.classList.remove("active");
     });
-});
-
+  });
