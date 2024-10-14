7b01f61035c337b248d75c7c14f4fb8a02d14a3a
Foxrim <Flavien085@gmail.com>
2024-10-14 11:27:22 +0200

    [fix] - Mise en page par prettier et correction de bug js sur les cards

--- scripts/cards.js
+++ scripts/cards.js
@@ -16,13 +16,3 @@ breakpoints: {
 } );
   splide.mount();
 } );
-
-new Splide( '.splide', {
-  arrowPath: 'm15.5 0.932-4.3 4.38...',
-  classes: {
-		arrows: 'splide__arrows your-class-arrows',
-		arrow : 'splide__arrow your-class-arrow',
-		prev  : 'splide__arrow--prev class-prev',
-		next  : 'splide__arrow--next class-next',
-  },
-} ).mount();
