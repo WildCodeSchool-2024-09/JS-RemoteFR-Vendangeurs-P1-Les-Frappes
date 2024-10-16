6c7a0ff206c88954a4650f93d84a6871c7329d10
Foxrim <Flavien085@gmail.com>
2024-10-16 23:57:57 +0200

    j'abandonne

--- scripts/script.js
+++ scripts/script.js
@@ -1,29 +1,44 @@
 import createCard from "./createCard.js";
 import restaurant from "./restaurant.js";
 
-const cardsContainer = document.querySelector('#cards-container');
-const searchInput = document.querySelector('#search-restaurant');
-const checkBoxFrance = document.querySelector('#check-france');
-const checkBoxAsie = document.querySelector('#check-Asie');
-const checkBoxVegan = document.querySelector('#check-vegan');
+document.addEventListener("DOMContentLoaded", () => {
+  const cardsContainer = document.querySelector("#cards-container");
+  const searchInput = document.querySelector("#search-restaurant");
+  const prevButton = document.querySelector(".prev");
+  const nextButton = document.querySelector(".next");
+  let currentIndex = 0;
 
-
-function displayCards() {
-
+  function displayCards() {
     const searchResto = searchInput.value;
 
-    const filterRestaurant = restaurant.filter(({name}) => {
-        const matchSearch = name.includes(searchResto)
+    const filterRestaurant = restaurant.filter(({ name }) => {
+      const matchSearch = name.includes(searchResto);
 
-        return matchSearch;
-    }
-)
+      return matchSearch;
+    });
 
-filterRestaurant.forEach((resto) => {
-    cardsContainer.appendChild(createCard(resto));
-})
-}
+    filterRestaurant.forEach((resto) => {
+      cardsContainer.appendChild(createCard(resto));
+    });
+  }
+
+  function updateCard() {
+    const translateX = -currentIndex * 33.33;
+    cardsContainer.style.transform = `translateX(${translateX}%)`;
+  }
+
+  prevButton.addEventListener("click", () => {
+    currentIndex = currentIndex > 0 ? currentIndex - 1 : restaurant.length - 1;
+    updateCard();
+  });
+
+  nextButton.addEventListener("click", () => {
+    currentIndex = currentIndex < restaurant.length - 1 ? currentIndex + 1 : 0;
+    updateCard();
+  });
 
 searchInput.addEventListener("input", displayCards);
 
-displayCards()
+displayCards();
+});
+
